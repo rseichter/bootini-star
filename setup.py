890caf9952be84e84e4d4ceb052f67c5a7be6960
Ralph Seichter <github@seichter.de>
2018-04-28 18:03:48 +0200

    Updated project URL

--- setup.py
+++ setup.py
@@ -9,14 +9,7 @@ setup(
     license='MIT',
     packages=['bootini_star', 'swagger_client'],
     include_package_data=True,
-    #   install_requires=[
-    #       'Flask >= 0.12.2',
-    #       'Flask-Login >= 0.4.1',
-    #       'Flask-Migrate >= 2.1.1',
-    #       'Flask-WTF >= 0.14.2',
-    #       'SQLAlchemy >= 1.2.6'
-    #   ],
-    classifiers=[  # Optional
+    classifiers=[
         'License :: OSI Approved :: MIT License',
         # How mature is this project? Common values are
         #   3 - Alpha
@@ -25,7 +18,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3.6',
     ],
-    project_urls={  # Optional
-        'Source': 'https://rseichter@bitbucket.org/rseichter/bootini-star.git',
+    project_urls={
+        'Source': 'https://github.com/rseichter/bootini-star.git'
     },
 )
